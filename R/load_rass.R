5550cbbf1e2834451e3409611e851d39c2118e15
ageneslaw <andy.geneslaw@gmail.com>
2024-01-10 22:36:11 -0500

    load_rass
    Created function and documentation for loading in RASS (richmond agitation sedation scale) data

---
+++ R/load_rass.R
@@ -0,0 +1,72 @@
+#' load_rass
+#'
+#' Load Richmond Agitation Sedation Scale data from Epic flowsheets.
+#' The datafile contains other types of flowsheet data, such as delirium
+#' (CAPD) scores. These are just filtered out. No processing is done
+#' other than cleaning the dataset.
+#'
+#' @param rass_filepath Path to the RASS data
+#' @param rass_colpaths A list of cols() specifications.
+#'   Cols specifications are things like col_integer(), col_character(), and can be found within the 'readr' package documentation.
+#' @param max_load A number, the maximum number of rows to load. The default is infinity.
+#'
+#' @return A data frame with:
+#' \itemize{
+#'   \item \code{mrn}: Medical record number
+#'   \item \code{enc_id}: Encounter ID, renamed from PAT_ENC_CSN_ID
+#'   \item \code{rass_time}: Datetime when the RASS was recorded
+#'   \item \code{rass}: Integer value of RASS [-5, 4] representing
+#'    &nbsp;&nbsp;-5 Unarousable
+#'    &nbsp;&nbsp;-4 Deep sedation
+#'    &nbsp;&nbsp;-3 Moderate sedation
+#'    &nbsp;&nbsp;-2 Light sedation
+#'    &nbsp;&nbsp;-1 Drowsy
+#'    &nbsp;&nbsp;0 Alert and calm
+#'    &nbsp;&nbsp;1 Restless
+#'    &nbsp;&nbsp;2 Agitated
+#'    &nbsp;&nbsp;3 Very agitated
+#'    &nbsp;&nbsp;4 Combative
+#' }
+#'
+#' @export
+#'
+#' @examples
+load_rass <- function(rass_filepath,
+                      rass_colpaths = list(
+                           col_character(),   # PAT_ENC_CSN_ID
+                           col_character(),   # MRN
+                           col_skip(),        # FLOWSHEET_GROUP
+                           col_character(),   # COMMON_NAME
+                           col_skip(),        # FLOWSHEET_NAME
+                           col_number(),      # CUST_LIST_MAP_VALUE
+                           col_skip(),        # MEAS_VALUE
+                           col_skip(),        # UNITS
+                           col_character()    # RECORDED_TIME
+                      ),
+                      max_load = Inf)
+
+{
+     require(tidyverse)
+     require(janitor)
+     require(forcats)
+     require(lubridate)
+     require(stringr)
+     require(readr)
+
+     df_rass <- read_delim(rass_filepath,
+                           delim = '|',
+                           col_types = rass_colpaths,
+                           n_max = max_load) %>%
+          clean_names() %>%
+          filter(str_detect(common_name, '^RASS')) %>%
+          mutate(across(where(is.character), str_to_lower)) %>%
+          mutate(recorded_time = ymd_hms(recorded_time)) %>%
+          rename(enc_id = pat_enc_csn_id,
+                 rass_time = recorded_time,
+                 rass = cust_list_map_value) %>%
+          select(-common_name) %>%
+          relocate(mrn)
+
+     return(df_rass)
+
+}
