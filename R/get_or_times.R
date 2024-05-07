68c95f1ab047310c9f47292906068c28573a8ae1
ageneslaw <46007706+ageneslaw@users.noreply.github.com>
2024-05-07 16:33:26 -0400

    get_or_times

--- R/get_or_times.R
+++ R/get_or_times.R
@@ -126,88 +126,26 @@ get_or_times <- function(adt_filepath,
                  level_of_care = factor(level_of_care),
                  or = if_else(level_of_care == 'or', TRUE, FALSE, FALSE))
 
-     # Only keep transfers to or from OR locations
-     df_adt <- df_adt %>% filter(department_name %in% or_locations)
 
-     # Group patients by admission date. Determine when patients changed locations.
-     # We only need to use admits, discharges, and transfer_in events (can drop all others).
-     # The strategy is to check, row-by-row for each patient, if the location for the current
-     # row matches the location for the prior row. If they match, the patient did not move and
-     # we can ultimately drop this row. If they don't match, this involves a change of
-     # location.
-     # Pre-admit refers to the first row, which by definition can't match.
-     adt_temp <- df_adt %>%
-          filter(event_type %in% c('admit', 'discharge', 'transfer_in', 'transfer_out')) %>%
-          mutate(department_name = as.character(department_name)) %>%
-          group_by(mrn, enc_id) %>%
+
+     # Note: sometimes the start date and stop date are very far apart. This is almost always due to
+     # clerical error. There is really nothing to do about it. We could arbitrarily decide to use the
+     # OR stop date - 24 hours, or the OR start date + 24 hours as the cap. But it isn't consistent
+     # whether one or the other is more reliable, and they can both induce error so we will just use both.
+     df_adt_simple <- df_adt %>%
+          filter(or & event_type %in% c('admit', 'discharge', 'transfer_in', 'transfer_out')) %>%
           arrange(mrn, enc_id, adt_date) %>%
-          mutate(
-               # Location prior to this one. Set equal to pre_admit if it wasn't defined.
-               last_loc = lag(department_name, default = 'pre_admit'),
-
-               # Level of care prior to this one (OR or non-OR).
-               # Set equal to pre_admit if it wasn't defined.
-               last_care_level = lag(level_of_care, default = last(level_of_care)),
-               last_care_level = forcats::fct_na_value_to_level(last_care_level, level = 'pre_admit'),
-
-               # Create a classification for the last entry for a patient (which may or may
-               # not be the discharge
-               last_row = if_else(row_number() == dplyr::n(), TRUE, FALSE)) %>%
-
+          distinct(mrn, enc_id, adt_date, event_type, .keep_all = T) %>%
+          group_by(mrn, enc_id) %>%
+          mutate(or_start = if_else(event_type %in% c('transfer_in', 'admit'), TRUE, FALSE),
+                 or_stop = if_else(event_type %in% c('transfer_out', 'discharge'),  TRUE, FALSE)) %>%
           ungroup() %>%
-
-          # Only keep rows where the last level of care is different than the current level of care, which
-          # defines a "meaningful" (i.e. outside OR to OR, or vice versa).
-          # Keep first and last entries (usually admit/discharge) by default.
-          filter(event_type == 'admit' | event_type == 'discharge' | last_row |
-                      (as.character(last_care_level) != as.character(level_of_care)))
-
-
-     # Get a value for "last OR" which is TRUE if the prior row for this patient was a
-     # in the OR
-     adt_temp <- adt_temp %>% group_by(mrn, enc_id) %>%
-          mutate(
-               last_or = lag(or),
-               last_or = tidyr::replace_na(last_or, FALSE)) %>%
-          ungroup()
-
-     # Find the start and stop dates for the OR. There can be multiple per patient
-     # If a patient was discharged from the OR, then set this as the stop date
-     #   Whenever "last_or" is false and "or" is true, this is a transition that defines a new OR course.
-     #   Whenever "last_or" is true and "or" is false, this transition defines the end of a new OR course.
-     #   If the last event occurred within the OR, then this is also the stop of a OR course.
-     adt_or <- adt_temp %>%
           group_by(mrn, enc_id) %>%
-          arrange(mrn, enc_id, adt_date) %>%
-          mutate(or_start = if_else(!last_or & or, TRUE, FALSE, FALSE),
-                 or_stop = if_else(!or & last_or, TRUE, FALSE, FALSE),
-                 or_stop = if_else(or & row_number() == dplyr::n(), TRUE, or_stop, or_stop),
-                 any_or = if_else(any(or), TRUE, FALSE)) %>%
-          ungroup()
-
-
-     # Create a simple dataset of these OR events
-     # the "values_fn = list" just suppresses an annoying warning about duplicates.
-     # The output format is that each row will have an MRN, an event ID, and an OR start and the subsequent OR stop date.
-     # These windows of time can be used to filter any other results.
-     # If any event from another table occurred between a given or_start_date and or_stop_date, then
-     # the event happened within an OR.
-     # the start and stop dates are formatted as datetimes in case we need to be very specific about timepoints, but can also be
-     # use the "floor" for the start date, and the "ceiling" for the stop date, to ensure the entire duration is captured.
-     # the values_fn = list just suppresses an annoying warning about duplicates
-     adt_or_simple <- adt_or %>%
-          select(mrn, enc_id, or_start, or_stop, adt_date) %>%
-          mutate(or_start_date = if_else(or_start, adt_date, NA_POSIXct_),
-                 or_stop_date = if_else(or_stop, adt_date, NA_POSIXct_)) %>%
-          tidyr::pivot_longer(cols = c('or_start_date', 'or_stop_date'),
-                              names_to = 'or_event', values_to = 'or_event_date') %>%
-          filter(!is.na(or_event_date)) %>%
-          select(-or_start, -or_stop, -adt_date) %>%
-          tidyr::pivot_wider(id_cols = c('mrn', 'enc_id'),
-                             names_from = 'or_event',
-                             values_from = 'or_event_date',
-                             values_fn = list) %>%
-          tidyr::unnest(cols = c('or_start_date', 'or_stop_date'))
+          arrange(adt_date, .by_group = TRUE) %>%
+          mutate(or_start_date = if_else(or_start, adt_date, lag(adt_date)),
+                 or_stop_date = if_else(or_start, lead(adt_date), adt_date)) %>%
+          ungroup() %>%
+          distinct(mrn, enc_id, or_start_date, or_stop_date)
 
      return(adt_or_simple)
 }
