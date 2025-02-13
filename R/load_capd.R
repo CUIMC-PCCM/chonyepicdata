5e6df1b3b3116feebe52098911023e0d68ffc869
ageneslaw <46007706+ageneslaw@users.noreply.github.com>
2025-02-13 17:45:23 -0500

    Update load_capd.R

--- R/load_capd.R
+++ R/load_capd.R
@@ -5,7 +5,15 @@
 #' Columns must have the names defined in the dataset from November 2023 (for now).
 #'
 #' @param capd_filepath Path to the CAP-D data
-#' @param capd_colpaths A list of cols() specifications.
+#' @param col_mapping Defines mapping of columns to key variables that will be output. You can dynamically
+#' specify these mappings when calling the function.
+#' The default is:
+#'       PAT_ENC_CSN_ID -> enc_id
+#'       MRN -> mrn
+#'       DISPLAY_NAME -> component_name
+#'       MEASURE_VALUE -> component_value
+#'       RECORDED_TIME -> capd_time
+#' @param capd_coltypes A list of cols() specifications specifying how columns should be read.
 #'   Cols specifications are things like col_integer(), col_character(), and can be found
 #'   within the \code{\link[readr]{cols}} documentation from the \code{readr} package.
 #'   If this isn't working well you can send in col_guess().
@@ -24,17 +32,14 @@
 #' @export
 #'
 load_capd <- function(capd_filepath,
-                      capd_colpaths = list(
-                           col_character(),   # PAT_ENC_CSN_ID
-                           col_character(),   # MRN
-                           col_skip(),        # FLOWSHEET_GROUP
-                           col_character(),   # COMMON_NAME
-                           col_skip(),        # FLOWSHEET_NAME
-                           col_number(),      # CUST_LIST_MAP_VALUE
-                           col_skip(),        # MEAS_VALUE
-                           col_skip(),        # UNITS
-                           col_character()    # RECORDED_TIME
+                      col_mapping = list(
+                           enc_id = "pat_enc_csn_id",
+                           mrn = "mrn",
+                           component_name = "display_name",
+                           component_value = "measure_value",
+                           capd_time = "recorded_time"
                       ),
+                      capd_coltypes = NULL,
                       max_load = Inf)
 {
 
@@ -44,35 +49,87 @@ load_capd <- function(capd_filepath,
           capd_communicate <- capd_restless <- capd_inconsolable <- capd_movement <-
           capd_response_time <- capd <- NULL
 
+     # Read file with guessed column types unless specified
      df_capd <- read_delim(capd_filepath,
                            delim = '|',
-                           col_types = capd_colpaths,
+                           col_types = if (is.null(capd_coltypes)) cols(.default = col_guess()) else capd_coltypes,
                            n_max = max_load) %>%
-          clean_names() %>%
-          mutate(
-               capd_question = case_when(
-                    common_name == 'Does the child male eye contact with the caregiver?' ~ 'capd_eye_contact',
-                    common_name == 'Are the child\'s actions purposeful?' ~ 'capd_purposeful',
-                    common_name == 'Is the child aware of his/her surroundings?' ~ 'capd_aware',
-                    common_name == 'Does the child communicate needs and wants?' ~ 'capd_communicate',
-                    common_name == 'Is the child restless?' ~ 'capd_restless',
-                    common_name == 'Is the child inconsolable?' ~ 'capd_inconsolable',
-                    common_name == 'Is the child underactive = very little movement while awake?' ~ 'capd_movement',
-                    common_name == 'Does it take the child a long time to respond to interactions?' ~ 'capd_response_time'
-               )) %>%
-          filter(!is.na(capd_question)) %>%
-          mutate(across(where(is.character), str_to_lower)) %>%
-          mutate(recorded_time = lubridate::ymd_hms(recorded_time)) %>%
-          rename(enc_id = pat_enc_csn_id,
-                 capd_time = recorded_time,
-                 component_name = capd_question,
-                 component_value = cust_list_map_value) %>%
+          clean_names()
+
+     # Print column names to debug
+     # print("Columns in loaded data:")
+     # print(colnames(df_capd))
+
+     # Rename columns based on col_mapping
+     df_capd <- df_capd %>%
+          rename(
+               enc_id = !!col_mapping$enc_id,
+               mrn = !!col_mapping$mrn,
+               component_name = !!col_mapping$component_name,
+               component_value = !!col_mapping$component_value,
+               capd_time = !!col_mapping$capd_time
+          )
+
+     # Print column names after renaming
+     # print("Columns after renaming:")
+     # print(colnames(df_capd))
+
+     # Check if all columns in col_mapping exist in the data
+     missing_cols <- setdiff(names(col_mapping), colnames(df_capd))
+     if(length(missing_cols) > 0) {
+          stop(paste("The following columns were not found in the data: ", paste(missing_cols, collapse = ",")))
+     }
+
+
+
+     # # Map the columns based on user input (col_mapping)
+     # df_capd <- df_capd %>%
+     #      rename(
+     #           enc_id = !!sym(col_mapping$enc_id),
+     #           mrn = !!sym(col_mapping$mrn),
+     #                component_name = !!sym(col_mapping$component_name),
+     #                component_value = !!sym(col_mapping$component_value),
+     #                capd_time = !!sym(col_mapping$capd_time)
+     #           )
+
+     # # Remove unneeded columns
+     # df_capd <- df_capd %>%
+     #      select(all_of(names(col_mapping)))
+
+     # print(head(df_capd))
+
+     # Perform transformation into capd (summing across components)
+     df_capd <- df_capd %>%
+          mutate(capd_time = lubridate::ymd_hms(capd_time)) %>%
+          mutate(component_value = as.numeric(component_value)) %>%
+          mutate(component_name = stringr::str_to_lower(component_name)) %>%
+          filter(!is.na(component_value)) %>%
+          filter(component_name %in% c(
+               "does the child make eye contact with the caregiver?",
+               "are the child's actions purposeful?",
+               "is the child aware of his/her surroundings?",
+               "does the child communicate needs and wants?",
+               "is the child restless?",
+               "is the child inconsolable?",
+               "is the child underactive = very little movement while awake?",
+               "does it take the child a long time to respond to interactions?"
+          )) %>%
+          mutate(component_name = case_when(
+               component_name == "does the child make eye contact with the caregiver?" ~ "capd_component_eye_contact",
+               component_name == "are the child's actions purposeful?" ~ "capd_component_purposeful",
+               component_name == "is the child aware of his/her surroundings?" ~ "capd_component_aware",
+               component_name == "does the child communicate needs and wants?" ~ "capd_component_communicate",
+               component_name == "is the child restless?" ~ "capd_component_restless",
+               component_name == "is the child inconsolable?" ~ "capd_component_inconsolable",
+               component_name == "is the child underactive = very little movement while awake?" ~ "capd_component_movement",
+               component_name == "does it take the child a long time to respond to interactions?" ~ "capd_component_response_time"
+          )) %>%
+          filter(!is.na(component_name)) %>%
           tidyr::pivot_wider(id_cols = c(mrn, enc_id, capd_time),
-                      names_from = 'component_name',
-                      values_from = 'component_value',
-                      values_fill = NA) %>%
-          mutate(capd = capd_eye_contact + capd_purposeful + capd_aware + capd_communicate +
-                                    capd_restless + capd_inconsolable + capd_movement + capd_response_time) %>%
+                             names_from = "component_name",
+                             values_from = "component_value",
+                             values_fill = NA) %>%
+          mutate(capd = rowSums(select(., starts_with("capd_component_")), na.rm = TRUE)) %>%
           select(mrn, enc_id, capd_time, capd) %>%
           filter(!is.na(capd)) %>%
           arrange(mrn, enc_id, capd_time)
